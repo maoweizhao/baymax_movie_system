5cc0f4e186a22dd83240aa45ec133ad8ff4c3465
毛伟钊 <1016182328@qq.com>
2020-12-02 18:03:45 +0800

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='Navbar'></div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+.Navbar{
+    width: 100%;
+    height: 68px;
+    background-color: red;
+}
+</style>
---
+++ src/views/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <navbar></navbar>
+  </div>
+</template>
+<script>
+import Navbar from '@cp/Navbar.vue'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+<style lang='less' scoped>
+</style>
