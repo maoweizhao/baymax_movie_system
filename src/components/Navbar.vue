45d3247ff00225b42813a27b4af1acb94256bcae
maoweizhao <1016182328@qq.com>
2020-12-12 23:50:05 +0800

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,83 +1,80 @@
 <template>
-    <div class='Navbar'>
-        <div class="logo"><a href="javaScript:;"></a></div>
-        <div class="menu">
-            <ul>
-                <li><a href="javaScript:;">电影</a></li>
-                <li><a href="javaScript:;">电视剧</a></li>
-                <li><a href="javaScript:;">动漫</a></li>
-                <li><a href="javaScript:;">综艺</a></li>
-                <li><a href="javaScript:;">纪录片</a></li>
-            </ul>
-        </div>
-        <div class="search_box"> <Input search placeholder="" /></div>
-        <div class="user">
-        <Dropdown>
-         <Avatar src="https://i.loli.net/2017/08/21/599a521472424.jpg" size="50" />
-            <DropdownMenu slot="list">
-            <DropdownItem>登录</DropdownItem>
-            <DropdownItem>注册</DropdownItem>
-            <DropdownItem>注销</DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
-        </div>
+  <div class="Navbar">
+    <div class="logo"><a href="javaScript:;"></a></div>
+    <div class="search_box">
+        <input class="search_content" type="text" placeholder="" />
+        <div class="search_button">
+      </div>
     </div>
+    <div class="user">
+      <Dropdown>
+        <Avatar
+          src="https://i.loli.net/2017/08/21/599a521472424.jpg"
+          size="40"
+        />
+        <DropdownMenu slot="list">
+          <DropdownItem>登录</DropdownItem>
+          <DropdownItem>注册</DropdownItem>
+          <DropdownItem>注销</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-}
+export default {}
 </script>
 <style lang='less' scoped>
-@h:68px;
-@100:100%;
+@h: 68px;
 .mixins {
-    width: @100;
-    height: @100;
+  width: 100%;
+  height: 100%;
 }
-.Navbar{
-    display: flex;
+.Navbar {
+  min-width: 1125px;
+  display: flex;
+  height: @h;
+  background-color: #000000;
+  color: #ffffff;
+  padding: 0 20px;
+  .logo {
+    flex: 0.2;
     height: @h;
-    background-color: #FE5447;
-    justify-content: flex-start;
-    color:#ffffff;
-    .logo {
-      flex: 0.2;
-      height: @h;
-      a {
+    a {
       display: block;
       .mixins();
-      background: url('../assets/logo.png') no-repeat;
-      background-size: @100;
-      }
+      background: url("../assets/logo.png") no-repeat;
+      background-size: 180px @h;
     }
-    .menu {
-      flex: 0.4;
-      height: @h;
-      li {
-        width: 20%;
-        height: @h;
-        line-height: @h;
-        text-align: center;
-          float: left;
-          a {
-              color: #ffffff;
-          }
-      }
-      li:hover{
-          background-color: #64462E;
-      }
+  }
+  .search_box {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    line-height: @h;
+    flex: 0.6;
+    height: @h;
+    padding: 0 100px;
+    .search_content {
+        text-indent:20px;
+        width: 285px;
+        height: 40px;
+        border-radius: 40px 0 0 40px;
+        background: #fff;
+        transition: 0.3s linear;
     }
-    .search_box{
-        line-height: @h;
-        flex: 0.3;
-        height: @h;
-        padding: 0 80px;
+    .search_button {
+        width: 100px;
+        height: 40px;
+        border-radius: 0 40px 40px 0;
+        background-color: #F15B20;
     }
-    .user {
-        padding-left: 30px;
-        line-height: @h;
-        flex: 0.1;
-        height: @h;
-    }
+  }
+  .user {
+    padding-left: 30px;
+    line-height: @h;
+    flex: 0.2;
+    height: @h;
+  }
 }
 </style>
