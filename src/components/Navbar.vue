4b6fca78c9c05f7da0deb923d9834b91ddbaa395
maoweizhao <1016182328@qq.com>
2020-12-13 23:04:18 +0800

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,10 @@
   <div class="Navbar">
     <div class="logo"><a href="javaScript:;"></a></div>
     <div class="search_box">
-        <input class="search_content" type="text" placeholder="" />
-        <div class="search_button">
+      <input class="search_content" type="text" placeholder="" />
+      <div class="search_btn">
+        <span class="icon iconfont">&#xe62f;</span>
+        搜索
       </div>
     </div>
     <div class="user">
@@ -15,7 +17,7 @@
         <DropdownMenu slot="list">
           <DropdownItem>登录</DropdownItem>
           <DropdownItem>注册</DropdownItem>
-          <DropdownItem>注销</DropdownItem>
+          <DropdownItem>退出</DropdownItem>
         </DropdownMenu>
       </Dropdown>
     </div>
@@ -51,23 +53,34 @@ export default {}
     display: flex;
     justify-content: center;
     align-items: center;
-    line-height: @h;
     flex: 0.6;
     height: @h;
     padding: 0 100px;
     .search_content {
-        text-indent:20px;
-        width: 285px;
-        height: 40px;
-        border-radius: 40px 0 0 40px;
-        background: #fff;
-        transition: 0.3s linear;
+      text-indent: 20px;
+      width: 285px;
+      height: 40px;
+      border-radius: 40px 0 0 40px;
+      background: #fff;
+      transition: 0.3s linear;
+      border: 2px solid #f15b20;
     }
-    .search_button {
-        width: 100px;
-        height: 40px;
-        border-radius: 0 40px 40px 0;
-        background-color: #F15B20;
+    .search_content:hover {
+      width: 300px;
+    }
+    .search_btn {
+      width: 100px;
+      height: 40px;
+      border-radius: 0 40px 40px 0;
+      border: 2px solid #f15b20;
+      background-color: #f15b20;
+      line-height: 40px;
+      font-size: 15px;
+      padding: 0 15px;
+    }
+    .search_btn:hover {
+      background: #ff183e;
+      cursor: pointer;
     }
   }
   .user {
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import '@/assets/css/reset.css'
 import '@/plugins/iview.js'
 // 按需引用仍要导入样式
 import 'view-design/dist/styles/iview.css'
+// 引入阿里字体图标样式
+import '@/assets/icon/iconfont.css'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
